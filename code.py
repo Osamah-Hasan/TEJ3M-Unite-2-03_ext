6aed875c53a1917acbae788e569f5e9c80ba6dcb
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2025-02-26 22:12:10 -0500

    done

---
+++ code.py
@@ -0,0 +1,22 @@
+"""
+Created by: Osamah
+Created on: FEB 2025
+Created for: TEJ3M-Unit 2-01 LED loop
+"""
+
+import board
+import digitalio
+import time
+
+blink_time = 1
+
+# Set up the LED pin (change 'board.GP15' to the correct pin for your board)
+led = digitalio.DigitalInOut(board.GP15)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True  # Turn LED on
+    time.sleep(blink_time)
+
+    led.value = False  # Turn LED off
+    time.sleep(blink_time)
